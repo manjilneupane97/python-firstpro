9d034ed17feef3171f820b27ec415f0de3ffb172
manjilneupane97 <mane_csit2075@lict.edu.np>
2020-09-15 08:13:03 -0500

    Two Python programs

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-firstpro
 
 It's just a short demonstration of use of pycharm using ubuntu linux
+Contain two python programs
---
+++ function.py
@@ -0,0 +1,4 @@
+def function(Name,Age):
+    print("My name is "+Name+" And My age is "+str(Age))
+function("Hacker",25)
+function("Manjil",23)
